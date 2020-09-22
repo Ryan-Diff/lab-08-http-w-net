e3493b71619351bc35f99d9ecc0685264f4cdfed
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-09-21 17:17:40 -0700

    POST /echo

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,9 +4,18 @@ const app = require('../lib/app');
 describe('createResponse', () => {
     it(' returns the text "hi" with GET method', async() => {
         const response = await request(app)
-        .get('/');
+        .get('/')
 
         expect(response.text)
-        .toEqual('hi');
+        .toEqual('hi')
+    });
+
+    it('responds with status code 200 and text with POST', async() => {
+        const response = await request(app)
+        .post('/echo')
+        .send('hi')
+
+        expect(response.text)
+        .toEqual('hi')
     });
 });
