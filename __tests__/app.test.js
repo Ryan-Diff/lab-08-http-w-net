92d662f5f9f638509bd445aaf148a1176f50558a
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-09-21 17:11:14 -0700

    / GET

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,11 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
+    it(' returns the text "hi" with GET method', async() => {
+        const response = await request(app)
+        .get('/');
 
+        expect(response.text)
+        .toEqual('hi');
+    });
 });
